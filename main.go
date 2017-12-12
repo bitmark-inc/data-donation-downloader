7f36b6f58ce0fe9e0513b43834363377566ff862
Anh Nguyen <anhnguyen@bitmark.com>
2017-12-12 13:32:23 +0700

    New downloader tool which is downloading files from new assets server

--- main.go
+++ main.go
@@ -9,228 +9,31 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
-	"time"
 
-	"github.com/bitmark-inc/bitmarkd/util"
-	"github.com/bitmark-inc/go-bitmarklib"
-	"github.com/lemonlatte/go-registry"
-	log "github.com/sirupsen/logrus"
-	"golang.org/x/crypto/ed25519"
-	"golang.org/x/crypto/nacl/secretbox"
-)
-
-var (
-	seedNonce = [24]byte{
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	}
-	authSeedCountBM = [16]byte{
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0xe7,
-	}
-	encrSeedCountBM = [16]byte{
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0xe8,
-	}
+	sdk "github.com/bitmark-inc/bitmark-sdk-go"
 )
 
 var version = "X.x"
 
 var (
-	bitmarkApiUrl     = ""
-	bitmarkStorageUrl = ""
+	bitmarkApiUrl = ""
 )
 
-type TokenReqBody struct {
-	Account   string `json:"account"`
-	Timestamp string `json:"timestamp"`
-	Signature string `json:"signature"`
-}
+var offsetFile = ".offset"
 
-type Provenance struct {
-	TxId      string `json:"tx_id"`
-	Owner     string `json:"owner"`
-	Status    string `json:"status"`
-	CreatedAt string `json:"created_at"`
-}
-
-type Bitmark struct {
-	Id         string       `json:"id"`
-	HeadId     string       `json:"head_id"`
-	Owner      string       `json:"owner"`
-	Issuer     string       `json:"issuer"`
-	Head       string       `json:"head"`
-	Status     string       `json:"status"`
-	Provenance []Provenance `json:"provenance"`
-}
-
-func NewSeedFromHexString(seed string) ([32]byte, error) {
-	rootSeed := [32]byte{}
-	b, err := hex.DecodeString(seed)
-	if err != nil {
-		return rootSeed, err
-	}
-
-	if len(b) != 32 {
-		return rootSeed, fmt.Errorf("invalid length of bitmark account")
-	}
-	copy(rootSeed[:], b[:32])
-	return rootSeed, nil
-}
-
-func registerEncKey(authKey *bitmarklib.KeyPair, encKey *bitmarklib.EncrKeyPair) error {
-	accountNo := authKey.Account().String()
-	signatureOrigin := ed25519.Sign(authKey.PrivateKeyBytes(), encKey.PublicKey[:])
-	signature := hex.EncodeToString(signatureOrigin)
-
-	encKeyUrl := fmt.Sprintf("%s/v1/encryption_keys/%s", bitmarkApiUrl, accountNo)
-
-	encryptPubKey := hex.EncodeToString(encKey.PublicKey[:])
-
-	req := map[string]string{
-		"encryption_pubkey": encryptPubKey,
-		"signature":         signature,
-	}
-
-	var buf bytes.Buffer
-	e := json.NewEncoder(&buf)
-	err := e.Encode(req)
-	if err != nil {
-		return err
-	}
-
-	log.Println("Check the encryption key here: ", encKeyUrl)
-	resp, err := http.Post(encKeyUrl, "application/json", &buf)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		body := map[string]string{}
-		d := json.NewDecoder(resp.Body)
-		d.Decode(&body)
-		return fmt.Errorf(body["message"])
-	}
-	return nil
-}
-
-func getEncryptionKey(accountNo string) ([32]byte, error) {
-	encKey := [32]byte{}
-
-	encKeyUrl := fmt.Sprintf("%s/v1/encryption_keys/%s", bitmarkApiUrl, accountNo)
-	resp, err := http.Get(encKeyUrl)
-	if err != nil {
-		return encKey, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return encKey, err
-	}
-
-	var respBody map[string]string
-	d := json.NewDecoder(resp.Body)
-	err = d.Decode(&respBody)
-	if err != nil {
-		return encKey, err
-	}
-	senderKeyBytes, err := hex.DecodeString(respBody["encryption_pubkey"])
-	if err != nil {
-		return encKey, err
-	}
-
-	copy(encKey[:], senderKeyBytes)
-	return encKey, nil
-}
-
-func getSessionData(bitmarkId, owner string) (*bitmarklib.SessionData, error) {
-	sessionData := &bitmarklib.SessionData{}
-	sessionUrl, err := url.Parse(fmt.Sprintf("%s/v1/session/%s", bitmarkApiUrl, bitmarkId))
-	if err != nil {
-		return nil, err
-	}
-	v := url.Values{}
-	v.Set("account_no", owner)
-	sessionUrl.RawQuery = v.Encode()
-
-	resp, err := http.Get(sessionUrl.String())
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("session data request status not ok")
-	}
-
-	d := json.NewDecoder(resp.Body)
-	err = d.Decode(&sessionData)
-	if err != nil {
-		return nil, err
-	}
-
-	return sessionData, nil
-}
-
-func getStorageAccessToken(authKeyPair *bitmarklib.KeyPair) (string, error) {
-	ts := fmt.Sprintf("%d", time.Now().UnixNano())
-	tokenReqUrl := fmt.Sprintf("%s/s/api/token", bitmarkStorageUrl)
-	tokenReq := TokenReqBody{
-		Account:   authKeyPair.Account().String(),
-		Timestamp: ts,
-		Signature: hex.EncodeToString(ed25519.Sign(authKeyPair.PrivateKeyBytes(), []byte(ts))),
-	}
-
-	var buf bytes.Buffer
-	e := json.NewEncoder(&buf)
-	err := e.Encode(&tokenReq)
-	if err != nil {
-		return "", err
-	}
-
-	resp, err := http.Post(tokenReqUrl, "application/json", &buf)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return "", fmt.Errorf("token request is not ok")
-	}
-	var body map[string]string
-
-	d := json.NewDecoder(resp.Body)
-	err = d.Decode(&body)
-	if err != nil {
-		return "", err
-	}
-	token, ok := body["token"]
-	if !ok {
-		return "", fmt.Errorf("no token found in response")
-	}
-	return token, nil
-}
-
-func getEncryptedFile(bitmarkId, token string) ([]byte, error) {
-	encFileGetUrl := fmt.Sprintf("%s/s/assets/%s?token=%s", bitmarkStorageUrl, bitmarkId, token)
-	resp, err := http.Get(encFileGetUrl)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	encrpytedFileBytes, err := ioutil.ReadAll(resp.Body)
-	return encrpytedFileBytes, err
+type bitmark struct {
+	ID     string `json:"id"`
+	Issuer string `json:"issuer"`
+	Offset int    `json:"offset"`
 }
 
 func main() {
-	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
-
 	init := false
 	showVersion := false
 	seed := ""
@@ -239,7 +42,7 @@ func main() {
 	flag.BoolVar(&init, "init", false, "initialize the bitmark")
 	flag.BoolVar(&showVersion, "version", false, "show version number")
 	flag.StringVar(&seed, "account", "", "Bitmark Account")
-	flag.StringVar(&network, "network", "", "Network")
+	flag.StringVar(&network, "network", "live", "Network")
 	flag.StringVar(&datadir, "data-dir", "data", "Directory to store all data")
 	flag.Parse()
 
@@ -248,160 +51,106 @@ func main() {
 		return
 	}
 
-	log.Printf("Network: %s", strings.ToUpper(network))
-	inTest := false
+	log.Println("Network:", strings.ToUpper(network))
+
 	switch network {
 	case "devel":
 		bitmarkApiUrl = "https://api.devel.bitmark.com"
-		bitmarkStorageUrl = "https://storage.devel.bitmark.com"
-		inTest = true
 	case "test":
 		bitmarkApiUrl = "https://api.test.bitmark.com"
-		bitmarkStorageUrl = "https://assets.test.bitmark.com"
-		inTest = true
 	default:
 		bitmarkApiUrl = "https://api.bitmark.com"
-		bitmarkStorageUrl = "https://assets.bitmark.com"
 	}
 
 	if seed == "" {
-		log.Fatal("invalid account")
+		log.Fatalln("invalid account")
 	}
-
-	rootSeed, err := NewSeedFromHexString(seed)
+	core, err := hex.DecodeString(seed)
 	if err != nil {
-		log.Fatalf("Fail to generate root seed: %s", err.Error())
+		log.Fatalln("invalid account")
 	}
 
-	authSeed := secretbox.Seal([]byte{}, authSeedCountBM[:], &seedNonce, &rootSeed)
-	authKeyPair, err := bitmarklib.NewKeyPairFromSeed(authSeed, inTest, bitmarklib.ED25519)
+	var n sdk.Network
+	if network == "live" {
+		n = 0
+	} else {
+		n = 1
+	}
+
+	account, err := sdk.AccountFromCore(n, core)
 	if err != nil {
-		log.Fatalf("Fail to generate account key: %s", err.Error())
+		log.Fatalln("Cannot initialize account from seed, error:", err)
 	}
-	log.Printf("Auth Account: %s", authKeyPair.Account().String())
+	totalBitmarks := 100
+	lastOffset := -1
+	savedOffset := -1
 
-	encSeed := secretbox.Seal([]byte{}, encrSeedCountBM[:], &seedNonce, &rootSeed)
-	encKeyPair, err := bitmarklib.NewEncrKeyPairFromSeed(encSeed[:])
+	for totalBitmarks >= 100 {
+		accountPath := filepath.Join(datadir, account.AccountNumber())
+		os.MkdirAll(accountPath, 0755)
+
+		savedOffset = readFirstOffset(accountPath)
+
+		// Fetch available assets from API server
+		url := bitmarkApiUrl + "/v1/bitmarks?pending=false&owner=" + account.AccountNumber()
+		if lastOffset > 0 {
+			url = url + "&at=" + strconv.Itoa(lastOffset)
+		}
+
+		log.Println("Fetching bitmarks with url:", url)
+		response, err := http.DefaultClient.Get(url)
+		if err != nil {
+			log.Fatalln("Error when getting data from API server err:", err)
+		}
+
+		var bitmarkData struct {
+			Bitmarks []bitmark `json:"bitmarks"`
+		}
+
+		decoder := json.NewDecoder(response.Body)
+		err = decoder.Decode(&bitmarkData)
+		if err != nil {
+			log.Fatal("Invalid data from API server")
+		}
+
+		totalBitmarks = len(bitmarkData.Bitmarks)
+
+		// Save first offset for next time fetching
+		if lastOffset == -1 {
+			saveFirstOffset(accountPath, bitmarkData.Bitmarks[0].Offset)
+		}
+
+		lastOffset = bitmarkData.Bitmarks[totalBitmarks-1].Offset
+
+		// Loop for bitmarks and download
+		for i, bitmark := range bitmarkData.Bitmarks {
+			if bitmark.Offset <= savedOffset {
+				goto Done
+			}
+
+			log.Printf("%d/%d Downloading bitmark: %s", i+1, totalBitmarks, bitmark.ID)
+			downloadBitmark(account, bitmark, datadir)
+		}
+	}
+Done:
+	log.Println("Done")
+}
+
+func downloadBitmark(account *sdk.Account, b bitmark, datadir string) {
+	_, assetBytes, err := account.DownloadAsset(b.ID)
 	if err != nil {
-		log.Fatalf("Fail to generate encryption key: %s", err.Error())
-	}
-	log.Printf("Enc Public Key: %s", strings.ToUpper(hex.EncodeToString(encKeyPair.PublicKey[:])))
-
-	if init {
-		err := registerEncKey(authKeyPair, encKeyPair)
-		if err != nil {
-			log.Errorf("can not register encryption key: %s", err.Error())
-		} else {
-			log.Info("account registered")
-		}
-		return
-	}
-
-	// fetch all bitmarks belong to the account
-	regClient, err := registry.New(bitmarkApiUrl)
-	if err != nil {
-		log.Fatalf("can not create a registry client: %s", err.Error())
-	}
-	b, err := regClient.GetBitmarkByOwner(authKeyPair.Account().String(), false, false)
-	if err != nil {
-		log.Fatalf("can not get bitmarks from registry: %s", err.Error())
-	}
-	var bitmarks []Bitmark
-	err = json.Unmarshal(b, &bitmarks)
-	if err != nil {
-		log.Fatalf("fail to decode registry data: %s", err.Error())
-	}
-
-	if len(bitmarks) == 0 {
-		log.Println("No data found")
-		return
-	}
-
-	// for each possible bitmarks, download and extract it.
-	log.Println("Start fetching donor data...")
-	for _, bmk := range bitmarks {
-		log.Printf("Get data with bitmark id: %s", bmk.Id)
-		// FIXME: remove the additional request if
-		// the registery supports `previous_owner` in the future
-		b, err := regClient.GetBitmark(bmk.Id, false, true)
-		if err != nil {
-			log.Fatalf("can not get bitmarks from registry: %s", err.Error())
-		}
-
-		err = json.Unmarshal(b, &bmk)
-		if err != nil {
-			log.Fatalf("fail to decode registry data: %s", err.Error())
-		}
-
-		var senderAccountNo string
-		if bmk.Owner == bmk.Issuer {
-			log.Info("The bitmark belongs to its issuer")
-			senderAccountNo = bmk.Provenance[len(bmk.Provenance)-1].Owner
-		} else {
-			// The items of the provenance is in descending order
-			senderAccountNo = bmk.Provenance[1].Owner
-		}
-
-		// Get encryption key
-		senderEncPubKey, err := getEncryptionKey(senderAccountNo)
-		if err != nil {
-			log.Printf("can not get the encryption key for an account. error: %s", err.Error())
-			continue
-		}
-
-		// Get session data
-		sessionData, err := getSessionData(bmk.Id, bmk.Owner)
-		if err != nil {
-			log.Warnf("fail to request session data. error: %s", err.Error())
-			continue
-		}
-
-		senderPubKey, err := bitmarklib.NewPublicKey(util.FromBase58(senderAccountNo))
-		if err != nil {
-			log.Warnf("fail to generate sender public key: %s", err.Error())
-			continue
-		}
-
-		// Decrypt session key
-		sessionKey, err := bitmarklib.SessionKeyFromSessionData(sessionData,
-			&senderEncPubKey, encKeyPair.PrivateKey,
-			senderPubKey.PublicKeyBytes())
-		if err != nil {
-			log.Warnf("unable to decrypt session data: %s", err.Error())
-			continue
-		}
-
-		token, err := getStorageAccessToken(authKeyPair)
-		if err != nil {
-			log.Warnf("unable to get storage token: %s", err.Error())
-			continue
-		}
-
-		encryptedFileBytes, err := getEncryptedFile(bmk.Id, token)
-		if err != nil {
-			log.Warnf("unable to get donor data: %s", err.Error())
-			continue
-		}
-
-		// Decrypt
-		donorData, err := bitmarklib.DecryptAssetFile(encryptedFileBytes, sessionKey,
-			senderPubKey.PublicKeyBytes())
-		if err != nil {
-			log.Warnf("unable to decrypt donor data: %s", err.Error())
-			continue
-		}
-
+		log.Println("Cannot download bitmark data: err:", err)
+	} else {
 		// unarchive the donor data
-		donorDatabuf := bytes.NewReader(donorData)
+		donorDatabuf := bytes.NewReader(assetBytes)
 		unzipper, err := zip.NewReader(donorDatabuf, int64(donorDatabuf.Len()))
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		datapath := filepath.Join(datadir, account.AccountNumber(), b.Issuer, b.ID)
+		os.MkdirAll(datapath, 0755)
 		for _, srcFile := range unzipper.File {
-			datapath := filepath.Join(datadir, bmk.Issuer, bmk.Id)
-			os.MkdirAll(datapath, 0755)
-
 			p := filepath.Join(datapath, srcFile.Name)
 			if srcFile.FileInfo().IsDir() {
 				os.MkdirAll(p, srcFile.Mode())
@@ -424,4 +173,36 @@ func main() {
 			}
 		}
 	}
+}
+
+func saveFirstOffset(path string, offset int) {
+	os.MkdirAll(path, 0755)
+	p := filepath.Join(path, offsetFile)
+	fo, err := os.Create(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fo.Close()
+
+	s := strconv.Itoa(offset)
+
+	_, err = io.Copy(fo, strings.NewReader(s))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func readFirstOffset(path string) int {
+	p := filepath.Join(path, offsetFile)
+	data, err := ioutil.ReadFile(p)
+	if err != nil {
+		return -1
+	}
+
+	offsetString := string(data)
+	offset, err := strconv.Atoi(offsetString)
+	if err != nil {
+		return -1
+	}
+	return offset
 }
